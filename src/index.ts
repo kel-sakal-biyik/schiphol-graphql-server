d2e68848c5509cc758aa407f3792d3f62456f41a
Önder Ceylan <onderceylan@gmail.com>
2018-02-18 20:29:34 +0100

    Revert "Fixed typing for the flightList"
    This reverts commit a64006ee66c19340d657179e3794be442a4fd450.

--- src/index.ts
+++ src/index.ts
@@ -22,5 +22,5 @@ app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 app.listen(9999, () => {
     console.log('GraphQL server is running on http://localhost:9999/graphql');
-    console.log('To explore your schema, visit http://localhost:9999/graphiql');
+    console.log('To explore your schema, visit http://localhost:9999/grapihql');
 });
